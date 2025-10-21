adaef4fecd52baf46ad855e82c8a0d29ac904694
Wilson <wsue1@my.bcit.ca>
2025-10-21 01:26:09 -0700

    Consolidate eslint config files, and update package

--- Frontend/eslint.config.js
+++ Frontend/eslint.config.js
@@ -1,24 +1,42 @@
 import globals from 'globals';
 import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
 import parser from '@typescript-eslint/parser';
+import reactPlugin from 'eslint-plugin-react';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default [
   {
-    files: ['*.{js,jsx,ts,tsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2022,
       sourceType: 'module',
       parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
       globals: {
         ...globals.browser,
-        myCustomGlobal: 'readonly',
+        ...globals.es2021,
       },
     },
     plugins: {
       '@typescript-eslint': typescriptEslintPlugin,
+      react: reactPlugin,
+      prettier: prettierPlugin,
     },
     rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...prettierConfig.rules,
       semi: ['error', 'always'],
+      'prettier/prettier': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
 ];
